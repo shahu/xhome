795fdce30ed0a482bcfa168ef2b0f22bb74cc5b7
alicexu <alicexu@pptv.com>
2013-09-08 21:39:53 +0800

    add show camera view.

--- app/XhomeCamera/src/com/xhome/camera/activity/CameraListActivity.java
+++ app/XhomeCamera/src/com/xhome/camera/activity/CameraListActivity.java
@@ -1,24 +1,17 @@
-
 package com.xhome.camera.activity;
 
-import com.nostra13.universalimageloader.core.DisplayImageOptions;
-import com.nostra13.universalimageloader.core.ImageLoader;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
-import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
-import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
-import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
-import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
-import com.xhome.camera.R;
-import com.xhome.camera.model.Constants;
-import com.xhome.camera.utils.FileUtils;
-import com.xhome.camera.utils.StringUtils;
-import com.xhome.camera.widget.RefreshListView;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.DialogInterface.OnDismissListener;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,237 +26,267 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
+import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.xhome.camera.R;
+import com.xhome.camera.model.Constants;
+import com.xhome.camera.utils.FileUtils;
+import com.xhome.camera.utils.StringUtils;
+import com.xhome.camera.widget.RefreshListView;
 
 public class CameraListActivity extends Activity {
 
-    private static final String TAG = CameraListActivity.class.getSimpleName();
+	private static final String TAG = CameraListActivity.class.getSimpleName();
 
-    private RefreshListView refreshListView;
+	private RefreshListView refreshListView;
 
-    DisplayImageOptions options;
+	DisplayImageOptions options;
 
-    protected ImageLoader imageLoader = ImageLoader.getInstance();
+	protected ImageLoader imageLoader = ImageLoader.getInstance();
 
-    private Button btnAdd;
+	private Button btnAdd;
 
-    private TextView txtTitle;
+	private TextView txtTitle;
 
-    private EditText cidEditText;
+	private EditText cidEditText;
 
-    CameraAdapter cameraAdapter;
+	CameraAdapter cameraAdapter;
 
-    AlertDialog alertDialog;
+	AlertDialog alertDialog;
 
-    AlertDialog.Builder builder;
+	AlertDialog.Builder builder;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
-        setContentView(R.layout.activity_camera_list);
-        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.camera_list_title);
-        init();
-        imageLoader.init(ImageLoaderConfiguration.createDefault(this));
-        options = new DisplayImageOptions.Builder().showImageOnLoading(R.drawable.ic_launcher)
-        .showImageForEmptyUri(R.drawable.ic_launcher)
-        .showImageOnFail(R.drawable.ic_launcher).cacheInMemory(true).cacheOnDisc(true)
-        .displayer(new RoundedBitmapDisplayer(20)).build();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+		setContentView(R.layout.activity_camera_list);
+		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
+				R.layout.camera_list_title);
+		init();
+		imageLoader.init(ImageLoaderConfiguration.createDefault(this));
+		options = new DisplayImageOptions.Builder()
+				.showImageOnLoading(R.drawable.ic_launcher)
+				.showImageForEmptyUri(R.drawable.ic_launcher)
+				.showImageOnFail(R.drawable.ic_launcher).cacheInMemory(true)
+				.cacheOnDisc(true).displayer(new RoundedBitmapDisplayer(20))
+				.build();
+	}
 
-    private void init() {
-        refreshListView = (RefreshListView)findViewById(R.id.list_camera);
-        btnAdd = (Button)findViewById(R.id.btn_add);
-        txtTitle = (TextView)findViewById(R.id.txt_title);
-        txtTitle.setText(R.string.camera_list);
-        List<String> list = getCameraList();
-        cameraAdapter = new CameraAdapter(list);
-        refreshListView.setAdapter(cameraAdapter);
-        initDialog();
-        btnAdd.setOnClickListener(new View.OnClickListener() {
+	private void init() {
+		refreshListView = (RefreshListView) findViewById(R.id.list_camera);
+		btnAdd = (Button) findViewById(R.id.btn_add);
+		txtTitle = (TextView) findViewById(R.id.txt_title);
+		txtTitle.setText(R.string.camera_list);
+		List<String> list = getCameraList();
+		cameraAdapter = new CameraAdapter(list);
+		refreshListView.setAdapter(cameraAdapter);
+		initDialog();
+		btnAdd.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-            public void onClick(final View v) {
-                builder.show();
-            }
-        });
-    }
+			@Override
+			public void onClick(final View v) {
+				alertDialog.show();
+			}
+		});
+	}
 
-    private void initDialog() {
-        LayoutInflater inflater = getLayoutInflater();
-        final View layout = inflater.inflate(R.layout.add_camera,
-                                             (ViewGroup)findViewById(R.id.dialog));
-        cidEditText = (EditText)layout.findViewById(R.id.cidEditText);
-        builder = new AlertDialog.Builder(this);
-        builder.setTitle("请输入");
-        builder.setIcon(android.R.drawable.ic_dialog_info);
-        builder.setView(layout);
-        builder.setPositiveButton("确定", new OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                String item = cidEditText.getText() + Constants.SEPARATE
-                              + Constants.IMAGES[0];
+	private void initDialog() {
+		LayoutInflater inflater = getLayoutInflater();
+		final View layout = inflater.inflate(R.layout.add_camera,
+				(ViewGroup) findViewById(R.id.dialog));
+		cidEditText = (EditText) layout.findViewById(R.id.cidEditText);
+		builder = new AlertDialog.Builder(this);
+		builder.setTitle("请输入");
+		builder.setIcon(android.R.drawable.ic_dialog_info);
+		builder.setView(layout);
+		builder.setPositiveButton("确定", new OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				String item = cidEditText.getText() + Constants.SEPARATE
+						+ Constants.IMAGES[0];
 
-                try {
-                    FileUtils.saveContentToLocal(CameraListActivity.this,
-                                                 Constants.FILE_NAME, item);
+				try {
+					FileUtils.saveContentToLocal(CameraListActivity.this,
+							Constants.FILE_NAME, item);
 
-                } catch(IOException e) {
-                    Log.e(TAG, "" + e);
-                }
+				} catch (IOException e) {
+					Log.e(TAG, "" + e);
+				}
 
-                dialog.dismiss();
-            }
-        });
-        builder.setNegativeButton("取消", new OnClickListener() {
+				dialog.dismiss();
+			}
+		});
+		builder.setNegativeButton("取消", new OnClickListener() {
 
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        alertDialog = builder.create();
-        alertDialog.setOnDismissListener(new OnDismissListener() {
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				dialog.dismiss();
+			}
+		});
+		alertDialog = builder.create();
+		alertDialog.setOnDismissListener(new OnDismissListener() {
 
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                List<String> items = getCameraList();
-                cameraAdapter.items = items;
-                cameraAdapter.notifyDataSetChanged();
-            }
-        });
-    }
-    /* (non-Javadoc)
-     * @see android.app.Activity#onResume()
-     */
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "activity onResume");
-    }
+			@Override
+			public void onDismiss(DialogInterface dialog) {
+				List<String> items = getCameraList();
+				cameraAdapter.items = items;
+				cameraAdapter.notifyDataSetChanged();
+			}
+		});
+	}
 
-    /*
-     * (non-Javadoc)
-     * @see android.app.Activity#onDestroy()
-     */
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        AnimateFirstDisplayListener.displayedImages.clear();
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onResume()
+	 */
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Log.d(TAG, "activity onResume");
+	}
 
-    private List<String> getCameraList() {
-        List<String> cameraList = new ArrayList<String>();
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		AnimateFirstDisplayListener.displayedImages.clear();
+	}
 
-        try {
-            cameraList = FileUtils.readFileFromData(this, Constants.FILE_NAME);
+	private List<String> getCameraList() {
+		List<String> cameraList = new ArrayList<String>();
 
-        } catch(IOException e) {
-            Log.e(TAG, "" + e);
-        }
+		try {
+			cameraList = FileUtils.readFileFromData(this, Constants.FILE_NAME);
 
-        Log.d(TAG, "list size:" + cameraList.size());
-        return cameraList;
-    }
+		} catch (IOException e) {
+			Log.e(TAG, "" + e);
+		}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		Log.d(TAG, "list size:" + cameraList.size());
+		return cameraList;
+	}
 
-    class CameraAdapter extends BaseAdapter {
-        public List<String> items;
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-        private final ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
+	class CameraAdapter extends BaseAdapter {
+		public List<String> items;
 
-        private class ViewHolder {
-            public TextView text;
+		private final ImageLoadingListener animateFirstListener = new AnimateFirstDisplayListener();
 
-            public ImageView image;
-        }
+		private class ViewHolder {
+			public TextView text;
 
-        public CameraAdapter(List<String> items) {
-            this.items = items;
-        }
+			public ImageView image;
+		}
 
-        /*
-         * (non-Javadoc)
-         * @see android.widget.Adapter#getCount()
-         */
-        @Override
-        public int getCount() {
-            return items.size();
-        }
+		public CameraAdapter(List<String> items) {
+			this.items = items;
+		}
 
-        /*
-         * (non-Javadoc)
-         * @see android.widget.Adapter#getItem(int)
-         */
-        @Override
-        public Object getItem(int position) {
-            return position;
-        }
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see android.widget.Adapter#getCount()
+		 */
+		@Override
+		public int getCount() {
+			return items.size();
+		}
 
-        /*
-         * (non-Javadoc)
-         * @see android.widget.Adapter#getItemId(int)
-         */
-        @Override
-        public long getItemId(int position) {
-            return position;
-        }
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see android.widget.Adapter#getItem(int)
+		 */
+		@Override
+		public Object getItem(int position) {
+			return position;
+		}
 
-        /*
-         * (non-Javadoc)
-         * @see android.widget.Adapter#getView(int, android.view.View,
-         * android.view.ViewGroup)
-         */
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view = convertView;
-            ViewHolder holder;
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see android.widget.Adapter#getItemId(int)
+		 */
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
 
-            if(null == convertView) {
-                view = getLayoutInflater().inflate(R.layout.item_list_image, null);
-                holder = new ViewHolder();
-                holder.text = (TextView)view.findViewById(R.id.text);
-                holder.image = (ImageView)view.findViewById(R.id.image);
-                view.setTag(holder);
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see android.widget.Adapter#getView(int, android.view.View,
+		 * android.view.ViewGroup)
+		 */
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			ViewHolder holder;
 
-            } else {
-                holder = (ViewHolder)view.getTag();
-            }
+			if (null == convertView) {
+				view = getLayoutInflater().inflate(R.layout.item_list_image,
+						null);
+				view.setOnClickListener(new View.OnClickListener() {
+					public void onClick(View v) {
+						Intent intent = new Intent(getApplicationContext(),
+								CameraShowActivity.class);
+						intent.putExtra("cid", "1");
+						startActivity(intent);
+					}
+				});
+				holder = new ViewHolder();
+				holder.text = (TextView) view.findViewById(R.id.text);
+				holder.image = (ImageView) view.findViewById(R.id.image);
+				view.setTag(holder);
 
-            holder.text.setText(StringUtils.getCameraName(items.get(position)));
-            String url = StringUtils.getCameraUrl(items.get(position));
-            Log.d(TAG, "imageUrl:" + url);
-            imageLoader.displayImage(url, holder.image, options, animateFirstListener);
-            return view;
-        }
-    }
+			} else {
+				holder = (ViewHolder) view.getTag();
+			}
 
-    private static class AnimateFirstDisplayListener extends SimpleImageLoadingListener {
+			holder.text.setText(StringUtils.getCameraName(items.get(position)));
+			String url = StringUtils.getCameraUrl(items.get(position));
+			Log.d(TAG, "imageUrl:" + url);
+			imageLoader.displayImage(url, holder.image, options,
+					animateFirstListener);
+			return view;
+		}
+	}
 
-        static final List<String> displayedImages = Collections
-                .synchronizedList(new LinkedList<String>());
+	private static class AnimateFirstDisplayListener extends
+			SimpleImageLoadingListener {
 
-        @Override
-        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
-            if(loadedImage != null) {
-                ImageView imageView = (ImageView)view;
-                boolean firstDisplay = !displayedImages.contains(imageUri);
+		static final List<String> displayedImages = Collections
+				.synchronizedList(new LinkedList<String>());
 
-                if(firstDisplay) {
-                    FadeInBitmapDisplayer.animate(imageView, 500);
-                    displayedImages.add(imageUri);
-                }
-            }
-        }
-    }
+		@Override
+		public void onLoadingComplete(String imageUri, View view,
+				Bitmap loadedImage) {
+			if (loadedImage != null) {
+				ImageView imageView = (ImageView) view;
+				boolean firstDisplay = !displayedImages.contains(imageUri);
+
+				if (firstDisplay) {
+					FadeInBitmapDisplayer.animate(imageView, 500);
+					displayedImages.add(imageUri);
+				}
+			}
+		}
+	}
 
 }
---
+++ app/XhomeCamera/src/com/xhome/camera/activity/CameraShowActivity.java
@@ -0,0 +1,103 @@
+package com.xhome.camera.activity;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.xhome.camera.R;
+import com.xhome.camera.model.Constants;
+import com.xhome.camera.utils.Constants.Extra;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.VideoView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class CameraShowActivity extends Activity {
+	Button playButton;
+	VideoView videoView;
+	EditText rtspUrl;
+    String[] imageUrls;
+
+    DisplayImageOptions options;
+    protected ImageLoader imageLoader = ImageLoader.getInstance();
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.camera_show);
+		
+		videoView = (VideoView) findViewById(R.id.rtsp_player);
+		PlayRtspStream("rtsp://218.204.223.237:554/live/1/66251FC11353191F/e7ooqwcfbqjoo80j.sdp");	
+	        imageUrls = Constants.IMAGES;
+
+	        options = new DisplayImageOptions.Builder()
+	        .showImageOnLoading(R.drawable.ic_stub)
+	        .showImageForEmptyUri(R.drawable.ic_empty)
+	        .showImageOnFail(R.drawable.ic_error)
+	        .cacheInMemory(true)
+	        .cacheOnDisc(true)
+	        .bitmapConfig(Bitmap.Config.RGB_565)
+	        .build();
+
+	        Gallery gallery = (Gallery) findViewById(R.id.gallery);
+	        gallery.setAdapter(new ImageGalleryAdapter());
+	        gallery.setOnItemClickListener(new OnItemClickListener() {
+	            @Override
+	            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	                startImagePagerActivity(position);
+	            }
+	        });
+	}
+
+	private void PlayRtspStream(String rtspUrl) {
+		videoView.setVideoURI(Uri.parse(rtspUrl));
+		videoView.requestFocus();
+		videoView.start();
+	}
+	
+	 private void startImagePagerActivity(int position) {
+	        Intent intent = new Intent(this, ImagePagerActivity.class);
+	        intent.putExtra(Extra.IMAGES, imageUrls);
+	        intent.putExtra(Extra.IMAGE_POSITION, position);
+	        startActivity(intent);
+	    }
+
+	    private class ImageGalleryAdapter extends BaseAdapter {
+	        @Override
+	        public int getCount() {
+	            return imageUrls.length;
+	        }
+
+	        @Override
+	        public Object getItem(int position) {
+	            return position;
+	        }
+
+	        @Override
+	        public long getItemId(int position) {
+	            return position;
+	        }
+
+	        @Override
+	        public View getView(int position, View convertView, ViewGroup parent) {
+	            ImageView imageView = (ImageView) convertView;
+
+	            if(imageView == null) {
+	                imageView = (ImageView) getLayoutInflater().inflate(R.layout.item_gallery_image, parent, false);
+	            }
+	            String url = imageUrls[position];
+	            imageLoader.displayImage(url, imageView, options);
+	            return imageView;
+	        }
+	    }
+}
